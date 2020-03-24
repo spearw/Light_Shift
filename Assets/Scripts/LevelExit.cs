22ae2f592657a4fe19ddc73ab698cf262fb0ea47
spearw <spearw@gmail.com>
2020-03-24 11:28:42 -0400

    Added player tag

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -3,20 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class LevelExit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnTriggerEnter2D(Collider2D other){
-        Debug.Log("Exiting level");
-        SceneManager.LoadScene("LevelSelect");
+        if (other.gameObject.CompareTag("Player")){
+            SceneManager.LoadScene("LevelSelect");
+        }
     }
 }
---
+++ Assets/Scripts/OutOfBounds.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OutOfBounds : MonoBehaviour
+{
+
+    public GameObject grabObject;
+    void OnTriggerEnter2D(Collider2D other){
+        Debug.Log("Restarting Level");
+        if (other.gameObject.CompareTag("Player")){
+            grabObject = null;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+}
